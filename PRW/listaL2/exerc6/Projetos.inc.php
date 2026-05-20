39fd2a4878b3d15283995951d9e96f0bac8c9034
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-05-20 13:45:21 -0300

    terninado exerc6

--- PRW/listaL2/exerc6/Projetos.inc.php
+++ PRW/listaL2/exerc6/Projetos.inc.php
@@ -29,11 +29,9 @@ class Projetos
          <tr>
           <th>ID</th>
           <th>Orcamento</th>
-          <th>Data de Início</th>
-          <th>Horas de Execução</th>
          </tr>";
 
-  $sql = "SELECT * FROM $nomeTabela ORDER BY id ASC";
+  $sql = "SELECT id, orcamento FROM $nomeTabela ORDER BY id ASC";
 
   $resultado = $conexao->query($sql) or die($conexao->error);
 
@@ -44,18 +42,9 @@ class Projetos
    $orcamento = htmlentities($vetorRegistro[1], ENT_QUOTES, 'UTF-8');
    $orcamentoFormatado = number_format($orcamento, 2, ',', '.');
 
-   $dataInicio = htmlentities($vetorRegistro[2], ENT_QUOTES, 'UTF-8');
-
-   $vetorData = explode('-', $dataInicio);
-   $dataFormatada = $vetorData[2] . '/' . $vetorData[1] . '/' . $vetorData[0];
-
-   $horasExecucao = htmlentities($vetorRegistro[3], ENT_QUOTES, 'UTF-8');
-
    echo "<tr>
           <td>$id</td>
           <td>R$$orcamentoFormatado</td>
-          <td>$dataFormatada</td>
-          <td>$horasExecucao h</td>
          </tr>";
   }
   echo "</table>";
@@ -63,41 +52,17 @@ class Projetos
 
  function readDataInicio($conexao, $nomeTabela)
  {
-  $sql = "SELECT * FROM $nomeTabela WHERE data_inicio > '2020-01-01';";
+  $sql = "SELECT count(id) as total FROM $nomeTabela WHERE data_inicio > '2020-01-01';";
 
   $resultado = $conexao->query($sql) or die($conexao->error);
 
+  $qtdTotal = $resultado->fetch_assoc();
+
   if ($resultado->num_rows > 0) {
-   echo "<div id='resultados' class='w3-container'>";
-   echo "
-          <table class='w3-table w3-bordered w3-border w3-centered'>
-           <caption class='w3-border w3-xlarge w3-margin-top'> Dados dos projetos cadastrados </caption>
-           <tr>
-            <th>ID</th>
-             <th>Orcamento</th>
-             <th>Data de Início</th>
-             <th>Horas de Execução</th>
-           </tr>";
-
-   while ($vetorRegistro = $resultado->fetch_array()) {
-    // Para evitarmos o ataque do tipo XSS, usamos o comando de sanitização a seguir
-    $id      = htmlentities($vetorRegistro[0], ENT_QUOTES, 'UTF-8');
-    $orcamento = htmlentities($vetorRegistro[1], ENT_QUOTES, 'UTF-8');
-    $orcamentoFormatado = number_format($orcamento, 2, ',', '.');
-    $dataInicio = htmlentities($vetorRegistro[2], ENT_QUOTES, 'UTF-8');
-    $vetorData = explode('-', $dataInicio);
-    $dataFormatada = $vetorData[2] . '/' . $vetorData[1] . '/' . $vetorData[0];
-    $horasExecucao = htmlentities($vetorRegistro[3], ENT_QUOTES, 'UTF-8');
-
-    echo "<tr>
-           <td>$id</td>
-           <td>R$$orcamentoFormatado</td>
-           <td>$dataFormatada</td>
-           <td>$horasExecucao h</td>
-          </tr>";
-   }
-   echo "</table>";
-   echo "</div>";
+   echo "<div id='resultados' class='w3-container w3-center w3-margin-top'>
+   A quantidade de projetos posteriores a 01/01/2020 são: "
+   . $qtdTotal['total'] .
+   "</div>";
   } else {
    echo "<div id='resultado'>";
    echo " <p> Não existem projetos com data de início posterior a 01/01/2020. </p> ";
@@ -113,11 +78,11 @@ class Projetos
   $linhasExcluidas = $conexao->affected_rows;
 
   if ($linhasExcluidas > 0) {
-   echo "<div class='w3-container'>";
+   echo "<div class='w3-container w3-center w3-margin-top'>";
    echo " <p id='excluidos'> $linhasExcluidas projeto(s) excluído(s) com sucesso! </p>";
    echo "</div>";
   } else {
-   echo "<div id='resultados' class='w3-container'>";
+   echo "<div id='resultados' class='w3-container w3-center w3-margin-top'>";
    echo " <p> Não foi possível excluir os projetos. Verifique se existem projetos com horas de execução menores que 100 e orçamento menor que R$1000. </p>";
    echo "</div>";
   }
